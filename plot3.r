869d4afee696a2ac900ebefb24fd13c1836a10ad
xshawnyu <32907171+xshawnyu@users.noreply.github.com>
2018-01-08 09:29:28 -0500

    Delete plot3.r

--- plot3.r
+++
@@ -1,9 +0,0 @@
-
-png(file="Plot3", width=480, height=480)
-with(sub, plot(dt,sub$Sub_metering_1, type="n", xlab="", ylab="Energy sub metering"))
-with(sub,lines(dt, sub$Sub_metering_1, col="black"))
-with(sub,lines(dt, sub$Sub_metering_2, col="red"))
-with(sub,lines(dt, sub$Sub_metering_3, col="blue"))
-legend("topright", lty=1, col=c("black","red","blue"), 
-       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),cex=0.6)
-dev.off()
